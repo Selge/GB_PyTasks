a47327701ca80a776af35b184bf21d12bab317af
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-14 16:22:47 +0200

    Update GB_PyTask_1.py

--- GB_PyTask_1.py
+++ GB_PyTask_1.py
@@ -18,14 +18,14 @@ def match_pointer():
     match task_pointer:
         case '1':
             task_1()
-        # case '2':
-        #     task_2()
-        # case '3':
-        #     task_3()
-        # case '4':
-        #     task_4()
-        # case '5':
-        #     task_5()
+        case '2':
+            task_2()
+        case '3':
+            task_3()
+        case '4':
+            task_4()
+        case '5':
+            task_5()
         case 'q':
             print("Well, see ya later. Bye!")
             sys.exit()
@@ -47,7 +47,8 @@ def task_1():
     """)
 
     weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
-    day = int(input("Please, type in target weekday number:  "))
+    request = "Please, type in target weekday number:  "
+    day = number_request(request)
 
     if 5 >= day >= 1:
         print(f"No, {weekdays[day - 1]} (day {day}) is not a weekend. It's a business day, get back to work!")
@@ -55,44 +56,67 @@ def task_1():
         print(f"Yes, {weekdays[day - 1]} (day {day}) is a weekend. Have some rest!")
     else:
         print("Wrong day number! Use numbers in range 1 - 7!")
+        number_request(request)
 
     something_else()
 
 
 def task_2():
+    print("""GB Python homework. Stage 1. Task 2.
+        Задание 2.
+        Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
+        для всех значений предикат.
+        """)
     pass
 
 
 def task_3():
+    print("""GB Python homework. Stage 1. Task 3.
+        Задание 3.
+        Напишите программу, которая принимает на вход координаты точки (X и Y), 
+        причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+        в которой находится эта точка (или на какой оси она находится).
+
+        Пример:
+
+        - x=34; y=-30 -> 4
+        - x=2; y=4-> 1
+        - x=-34; y=-30 -> 3
+        """)
     pass
 
 
 def task_4():
+    print("""GB Python homework. Stage 1. Task 4.
+        Задание 4.
+        Напишите программу, которая по заданному номеру четверти, 
+        показывает диапазон возможных координат точек в этой четверти (x и y).
+        """)
     pass
 
 
 def task_5():
+    print("""GB Python homework. Stage 1. Task 5.
+        Задание 5.
+        Напишите программу, которая принимает на вход координаты двух точек 
+        и находит расстояние между ними в 2D пространстве.
+
+        Пример:
+
+        - A (3,6); B (2,1) -> 5,09
+        - A (7,-5); B (1,-1) -> 7,21
+        """)
+    pass
+
+
+def number_request(request):
+    number = int(input(str(request)))
+    return number
+
+
+def something_else():
     pass
 
 
 if __name__ == '__main__':
     start_menu()
-
-#   Задание 2. Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-#
-#  Задание 3. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
-#
-#     Пример:
-#
-# - x=34; y=-30 -> 4
-# - x=2; y=4-> 1
-# - x=-34; y=-30 -> 3
-#
-#  Задание 4. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-#
-#  Задание 5. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-#
-#     Пример:
-#
-# - A (3,6); B (2,1) -> 5,09
-# - A (7,-5); B (1,-1) -> 7,21
