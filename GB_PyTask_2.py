cf4aff4e2765bccfb857b74d990a30e07f7852f4
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-20 16:15:29 +0200

    Update GB_PyTask_2.py

--- GB_PyTask_2.py
+++ GB_PyTask_2.py
@@ -1,33 +1,4 @@
-import sys
-
-
-def start_menu():
-    print("""Welcome to GB Python homework #2.
-    Please, choose a task from the list below or push 'q' to exit the program:
-    - 1. Task 1
-    - 2. Task 2
-    - 3. Task 3
-    - 4. Task 4
-    - 5. Task 5
-    """)
-    match_pointer()
-
-def match_pointer():
-    task_pointer = str(symbol_request("Please, make your choice:  ")).lower()
-    match task_pointer:
-        case '1':
-            task_1()
-        case '2':
-            task_2()
-        case '3':
-            task_3()
-        case '4':
-            task_4()
-        case 'q':
-            sys.exit()
-        case _:
-            print("Please, use built-in options!")
-            match_pointer()
+from GB_PyTask_1 import symbol_request
 
 
 def task_1():
@@ -41,12 +12,10 @@ def task_1():
     print("GB Python homework. Stage 2. Task 1.")
 
     try:
-        number = float(symbol_request("Please, type in target weekday number:  "))
+        number = float(symbol_request("Please, type in target value:  "))
     except ValueError:
         print("Invalid data type!")
         task_1()
-
-    anything_else(task_1)
 
 
 def task_2():
@@ -64,7 +33,6 @@ def task_2():
         print("Invalid data type!")
         task_2()
 
-    anything_else(task_2)
 
 
 def task_3():
@@ -75,7 +43,6 @@ def task_3():
     # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
     print("GB Python homework. Stage 2. Task 3.")
-    anything_else(task_3)
 
 
 def task_4():
@@ -87,31 +54,3 @@ def task_4():
     # Реализуйте алгоритм перемешивания списка.
 
     print("GB Python homework. Stage 2. Task 4.")
-    anything_else(task_4)
-
-
-def symbol_request(request):
-    symbol = input(str(request))
-    return symbol
-
-
-def anything_else(task):
-    print("""
-    Do you want anything else?
-    Please, type:
-    'a' - to start the current task again
-    'm' - to get back to the main menu
-    'q' - to exit the program
-    """)
-    again = str(symbol_request("Please, make your choice:  ")).lower()
-    match again:
-        case 'a':
-            task()
-        case 'm':
-            start_menu()
-        case 'q':
-            sys.exit()
-
-
-if __name__ == '__main__':
-    start_menu()
